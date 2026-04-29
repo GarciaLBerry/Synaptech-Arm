4ae3043a2c1d2c90747ffb4872be6279a1acbea1
evost-uwt <evost@uw.edu>
2026-04-29 15:59:03 -0700

    Signal shaping, data formatting, temporary adjustments to run with model (#8)
    - Improved the processing of the raw signal into the correct expected shapes.
     - Changed the stored data to be a labeled dataframe as used in the model
     - Had to make two adjustments to the model that will be different in future versions:
       - In the saved joblib bundle, the object used for performing inference will be keyed to "pipeline" instead of "model" (Currently it is "model"
       - Current input data assumes taking in accelerometers as inputs in addition to the brain data. We will be removing that in future versions.

--- model/inference.py
+++ model/inference.py
@@ -1,20 +1,21 @@
 from . import config
 from . import utils
+import pandas as pd
+import numpy as np
 
 verbose = False
 
-def predict(x_test = None, y_test = None):
-    if (x_test is None) or (y_test is None):
-        data = utils.read_dataset_from_csv(config.dataset_path)
-        #utils.debug_print_dataset_details(data)
-        x_train, x_test, y_train, y_test = utils.get_split_data(data)
-        
+def predict(x_test, y_test = None, evaluate = False):
     model = utils.load_latest_pipeline()
     predictions = model.predict(x_test)
-    accuracy = (predictions == y_test).mean()
+    
+    if evaluate and (y_test is None):
+        data = utils.read_dataset_from_csv(config.dataset_path)
+        #utils.debug_print_dataset_details(data)
+        _, _, _, y_test = utils.get_split_data(data)
+        
     if verbose:
         print(f"Predictions: {predictions[0]}")
-        print(f"Model Accuracy: {accuracy:.4f}")
     
     # TODO: Make sure this actually return something useful here
     return predictions[0]
